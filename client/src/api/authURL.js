b891f562762008b212a6d9d13c28c957d122e3fb
vikramdev772 <vikramdev772@gmail.com>
2024-12-27 21:23:35 +0530

    fixing ui

--- client/src/api/authURL.js
+++ client/src/api/authURL.js
@@ -1,6 +1,6 @@
 const authUrl = 'https://uscl-auth.onrender.com/auth';
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// export default authUrl;
+// =======
 export default authUrl;
-=======
-export default authUrl;
->>>>>>> c555cfa4b25b5b0905ee86bae3b4e53d43980252
+// >>>>>>> c555cfa4b25b5b0905ee86bae3b4e53d43980252
